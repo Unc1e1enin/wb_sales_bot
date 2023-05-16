c2dc0578ecc095b0f7720afe9f8c848215c833f3
Unc1e1enin <133760448+Unc1e1enin@users.noreply.github.com>
2023-05-16 14:42:42 +0300

    Словарь со смайликами
    Словарь со смайликами, которые используются ботом для красочного предоставления информации в чате

---
+++ smiles.py
@@ -0,0 +1,20 @@
+smile = {
+    'shopping_cart': '\U0001F6D2',
+    'money_bag': '\U0001F4B0',
+    'link': '\U0001F517',
+    'globe': '\U0001F30D',
+    'calendar': '\U0001F4C6',
+    'time': '\U0001F55A',
+    'Колье': '\U0001F4FF',
+    'Кольца': '\U0001F48D',
+    'Серьги': '\U0001F9F7',
+    'Браслеты': '\U0001FAA2',
+    'Банты': '\U0001F380',
+    'id': '\U0001F194',
+    'credit_card': '\U0001F4B3',
+    'return': '\U0001F6AB',
+    'fly': '\U0001F4B8',
+    'red_car': '\U0001F69A',
+    'green_car': '\U0001F69B',
+    'box': '\U0001F4E6'
+}
